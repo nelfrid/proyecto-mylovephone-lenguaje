426531b56424ad9b9df147e36b536355ebe1f40f
Raichu7801 <oswelcardona94@gmail.com>
2026-05-25 13:32:48 -0500

    Solo es para poder hacer pull.

--- MyLoveStore/MyLoveStore/Formularios/Sistema Facturación/FormFacturaFinal.Designer.cs
+++ MyLoveStore/MyLoveStore/Formularios/Sistema Facturación/FormFacturaFinal.Designer.cs
@@ -88,7 +88,7 @@
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(222, 22);
+            this.pictureBox1.Location = new System.Drawing.Point(63, 22);
             this.pictureBox1.Name = "pictureBox1";
             this.pictureBox1.Size = new System.Drawing.Size(135, 58);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
