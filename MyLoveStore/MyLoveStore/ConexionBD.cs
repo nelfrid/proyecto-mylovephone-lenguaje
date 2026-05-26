64d25ae48261d52c0e52ab2e5f8c341c61b7b404
RolandoMontero777 <rolando.montero@utp.ac.pa>
2026-05-26 13:04:46 -0500

    Se agrego la clase conexionBD.cs para que se cumunicar la base de datos

---
+++ MyLoveStore/MyLoveStore/ConexionBD.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.OleDb;
+using System.IO;
+using System.Windows.Forms;
+
+namespace MyLoveStore
+{
+    public class ConexionBD
+    {
+        public static OleDbConnection ObtenerConexion()
+        {
+            string ruta = Path.Combine(Application.StartupPath, "Basedatos", "Inventario del proyecto2.accdb");
+
+            string cadena = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source={ruta};Persist Security Info=False;";
+
+            return new OleDbConnection(cadena);
+        }
+    }
+}
